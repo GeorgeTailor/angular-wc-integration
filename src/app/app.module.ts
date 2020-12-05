d8438ac1a17a1b083478b2c2193c39cd2f4be272
Yuriy Kravets <yucikkravets@gmail.com>
2020-12-05 13:03:05 +0100

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import "../../node_modules/@zooplus/zoo-web-components/dist/zoo-components-iife.js";
+import "../../node_modules/@zooplus/zoo-web-components/dist/zoo-components-esm.js";
 import { ReactiveFormsModule } from '@angular/forms';
 
 
